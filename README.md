d20ad454fdd85fce1f0fa4e03ce6068068298895
TobiasPleyer <tobi.pleyer@gmail.com>
2019-08-04 23:01:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# yampa-ball
+
+A very basic console animation of a falling ball in
+[Haskell](https://www.haskell.org/) using the
+[Yampa](https://hackage.haskell.org/package/Yampa) framework. This is meant as
+a simple example for functional reactive programming (FRP).
+
+The code is partly inspired by a youtube video of
+[jekor](https://www.youtube.com/watch?v=-IpE0CyHK7Q).
---
+++ src/Main.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import           FRP.Yampa
+
+type Pos = Double
+type Vel = Double
+
+-- Type signature of our signal function.
+-- It takes an initial position and then animates the ball falling to ground,
+-- accelerated by gravity.
+fallingBall :: Pos -> SF () (Pos,Vel)
+-- First iteration: The ball does not change position (no gravity)
+fallingBall pos = constant pos &&& constant 0.0
+
+main :: IO ()
+main = do
+  putStrLn "Hello FRP!"
+  -- The embed function calculates the outputs (effects) of a signal function
+  -- for a list of concrete times in the conceptually continous time line
+  print $ embed (fallingBall 10.0) ((), [ (0.0, Nothing)
+                                        , (1.0, Nothing)
+                                        , (2.0, Nothing)
+                                        , (3.0, Nothing)
+                                        ])
