4ed0aa4dbd6a9e8a50344308080951b7a2d889c6
TobiasPleyer <tobi.pleyer@gmail.com>
2019-08-05 22:53:45 +0200

    Integrate velocty over time

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,33 @@
 module Main where
 
+import           Control.Monad (forM_)
 import           FRP.Yampa
 
 type Pos = Double
 type Vel = Double
 
 -- Type signature of our signal function.
--- It takes an initial position and then animates the ball falling to ground,
--- accelerated by gravity.
+-- It takes an initial position (s0) and the intial velocity (v0) and then
+-- animates the ball falling to the ground, accelerated by gravity.
 fallingBall :: Pos -> SF () (Pos,Vel)
--- First iteration: The ball does not change position (no gravity)
-fallingBall pos = constant pos &&& (constant (-9.81) >>> integral)
+fallingBall y0 = (constant (-9.81) >>> integral) >>> ((integral >>^ (+y0)) &&& identity)
+--fallingBall y0 = constant 1 >>> integral >>> (arr (\t -> y0 - 4.905 * t**2) &&& arr (*(-9.81)))
 
 main :: IO ()
 main = do
   putStrLn "Hello FRP!"
   -- The embed function calculates the outputs (effects) of a signal function
   -- for a list of concrete times in the conceptually continous time line
-  print $ embed (fallingBall 10.0) ((), [ (0.0, Nothing)
-                                        , (0.5, Nothing)
-                                        , (1.0, Nothing)
-                                        , (1.5, Nothing)
-                                        , (2.0, Nothing)
-                                        , (2.5, Nothing)
-                                        , (3.0, Nothing)
-                                        ])
+  forM_ (embed (fallingBall 10.0)
+                ((),
+                [ (0.1, Nothing)
+                , (0.1, Nothing)
+                , (0.1, Nothing)
+                , (0.1, Nothing)
+                , (0.1, Nothing)
+                , (0.1, Nothing)
+                , (0.1, Nothing)
+                , (0.1, Nothing)
+                , (0.1, Nothing)
+                , (0.1, Nothing)
+                ])) print
